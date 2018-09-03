8ced110076a7bf73a7f186f940721628043ee826
Unknown <omar.elsayedmohamed@student.guc.edu.eg>
2018-09-04 01:58:48 +0200

    Main menu and the beginning of the first scene

---
+++ Spingere/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void QuitGame()
+    {
+        print("Quit");
+        Application.Quit();
+       
+    }
+}
---
+++ Spingere/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rigidMain;
+    [SerializeField]
+    public Slider mainSlider;
+    [SerializeField]
+    public Slider otherSlider;
+    // Use this for initialization
+    void Start () {
+        rigidMain = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+		
+	}
+
+
+    private void FixedUpdate()
+    {
+        Vector3 movingPosition = new Vector3(mainSlider.value, 0, 50);
+        transform.Translate(50 * Vector3.forward * Time.deltaTime);
+        transform.Translate(new Vector3(mainSlider.value, 0, 0) * Time.deltaTime * 4);
+
+
+    }
+
+
+
+
+
+    public void OnSliderChanger(float SteeringValue)
+    {
+      
+       // transform.position = new Vector3(SteeringValue, transform.position.y, transform.position.z);
+       
+
+    }
+}
